dd08c311f8f6fccf38e90b859b2b8a5ada587624
Solomon Demisse <soul_tefera@yahoo.com>
2026-02-27 15:26:42 -0600

    feat(main): create intial project structure

---
+++ src/main/java/com/mytest/loan/management/system/LoanManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mytest.loan.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementSystemApplication.class, args);
+	}
+
+}
